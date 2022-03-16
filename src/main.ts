abaa46af5bb8f3a80c367042dc22c39abc90eadb
Giovanni Bernini <giovannibernini.ti@gmail.com>
2022-03-16 13:22:48 -0300

    hotfix

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,31 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { exec } from '@actions/exec'
-import { comment } from './commentToPullRequest'
+import { comment as githubComment } from './commentToPullRequest'
 import { execSurgeCommand, formatImage } from './helpers'
 
-let failOnErrorGlobal = false
-let fail: (err: Error) => void
-
-async function main() {
-	const surgeToken = core.getInput('surge_token')
-	const previewUrl = core.getInput('preview_url')
-	const previewPath = core.getInput('preview_path')
-	const token = core.getInput('github_token', { required: true })
-	const distFolder = core.getInput('dist')
-	const teardown =
-		core.getInput('teardown')?.toString().toLowerCase() === 'true'
-
-	const failOnError = !!(
-		core.getInput('failOnError') || process.env.FAIL_ON__ERROR
-	)
-	failOnErrorGlobal = failOnError
-	core.debug(
-		`failOnErrorGlobal: ${typeof failOnErrorGlobal} + ${failOnErrorGlobal.toString()}`
-	)
-	const octokit = github.getOctokit(token)
-	let prNumber: string | number | undefined
-	core.debug('github.context')
-	core.debug(JSON.stringify(github.context, null, 2))
-	const { job, payload } = github.context
-	core.debug(`payload.after: ${payload.after}`)
-	core.debug(`payload.after: ${payload.pull_request}`)
+function getGitCommitSha(): string {
+	const { payload } = github.context
 	const gitCommitSha =
 		payload.after ||
 		payload?.pull_request?.head?.sha ||
 		payload?.workflow_run?.head_sha
-	core.debug(JSON.stringify(github.context.repo, null, 2))
-	const fromForkedRepo =
-		payload.pull_request?.owner === github.context.repo.owner
 
-	if (payload.number && payload.pull_request) {
-		prNumber = payload.number
-	} else {
+	return gitCommitSha
+}
+
+async function getPullRequestNumber(): Promise<number | undefined> {
+	const token = core.getInput('github_token', { required: true })
+	const octokit = github.getOctokit(token)
+	const { payload } = github.context
+	const gitCommitSha = getGitCommitSha()
+	const prNumberExists = payload.number && payload.pull_request
+
+	if (prNumberExists) {
+		return Number(payload.number)
+	}
+
+	if (!prNumberExists) {
 		const result =
 			await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
 				owner: github.context.repo.owner,
@@ -50,59 +35,38 @@ async function main() {
 		const pr = result.data.length > 0 && result.data[0]
 		core.debug('listPullRequestsAssociatedWithCommit')
 		core.debug(JSON.stringify(pr, null, 2))
-		prNumber = pr ? pr.number : undefined
+		const prNumber = pr ? Number(pr.number) : undefined
+		return prNumber
 	}
-	if (!prNumber) {
-		core.info(`😢 No related PR found, skip it.`)
+}
+
+async function comment(message: string): Promise<void> {
+	const { job, payload } = github.context
+	const prOwner = payload.pull_request?.owner
+	const fromForkedRepo = prOwner === github.context.repo.owner
+	const token = core.getInput('github_token', { required: true })
+	const octokit = github.getOctokit(token)
+	const prNumber = await getPullRequestNumber()
+
+	if (fromForkedRepo) {
 		return
 	}
-	core.info(`Find PR number: ${prNumber}`)
 
-	const commentIfNotForkedRepo = (message: string) => {
-		if (fromForkedRepo) {
-			return
-		}
-		comment({
-			repo: github.context.repo,
-			number: Number(prNumber),
-			message,
-			octokit,
-			header: job,
-		})
-	}
+	githubComment({
+		repo: github.context.repo,
+		number: Number(prNumber),
+		message,
+		octokit,
+		header: job,
+	})
+}
 
-	fail = (err: Error) => {
-		core.info('error message:')
-		core.info(JSON.stringify(err, null, 2))
-		const repoOwner = github.context.repo.owner
-		const repoName = github.context.repo.repo
-		const repoId = github.context.runId
-		const buildLogsUrl = `https://github.com/${repoOwner}/${repoName}/actions/runs/${repoId}`
-		const image = formatImage({
-			buildingLogUrl,
-			imageUrl:
-				'https://user-images.githubusercontent.com/507615/90250824-4e066700-de6f-11ea-8230-600ecc3d6a6b.png',
-		})
+async function generateLogUrl(): Promise<string> {
+	const token = core.getInput('github_token', { required: true })
+	const octokit = github.getOctokit(token)
+	const { job } = github.context
 
-		commentIfNotForkedRepo(
-			`😭 Deploy PR Preview ${gitCommitSha} failed. [Build logs](${buildLogsUrl}) \n ${image}`
-		)
-		if (failOnError) {
-			core.setFailed(err.message)
-		}
-	}
-
-	const repoOwner = github.context.repo.owner.replace(/\./g, '-')
-	const repoName = github.context.repo.repo.replace(/\./g, '-')
-	const url = previewUrl
-		.replace('{{repoOwner}}', repoOwner)
-		.replace('{{repoName}}', repoName)
-		.replace('{{job}}', job)
-		.replace('{{prNumber}}', `${prNumber}`)
-		.concat('.surge.sh')
-		.concat(previewPath)
-
-	core.setOutput('preview_url', url)
+	const gitCommitSha = getGitCommitSha()
 
 	let data
 	try {
@@ -113,8 +77,9 @@ async function main() {
 		})
 		data = result.data
 	} catch (err) {
-		fail(err)
-		return
+		core.info('generateLogUrl error')
+		await fail(err)
+		return ''
 	}
 
 	core.debug(JSON.stringify(data?.check_runs, null, 2))
@@ -128,6 +93,78 @@ async function main() {
 	const buildingLogUrl = checkRunId
 		? `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/runs/${checkRunId}`
 		: `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}`
+
+	return buildingLogUrl
+}
+
+async function fail(err: Error) {
+	core.info('error message:')
+	core.info(JSON.stringify(err, null, 2))
+	const repoOwner = github.context.repo.owner
+	const repoName = github.context.repo.repo
+	const repoId = github.context.runId
+	const buildLogsUrl = `https://github.com/${repoOwner}/${repoName}/actions/runs/${repoId}`
+	const buildingLogUrl = await generateLogUrl()
+
+	const gitCommitSha = getGitCommitSha()
+
+	const image = formatImage({
+		buildingLogUrl,
+		imageUrl:
+			'https://user-images.githubusercontent.com/507615/90250824-4e066700-de6f-11ea-8230-600ecc3d6a6b.png',
+	})
+
+	await comment(
+		`😭 Deploy PR Preview ${gitCommitSha} failed. [Build logs](${buildLogsUrl}) \n ${image}`
+	)
+
+	const failOnError = !!(
+		core.getInput('failOnError') || process.env.FAIL_ON__ERROR
+	)
+
+	if (failOnError) {
+		core.setFailed(err.message)
+	}
+}
+
+async function main() {
+	const surgeToken = core.getInput('surge_token')
+	const previewUrl = core.getInput('preview_url')
+	const previewPath = core.getInput('preview_path')
+	const distFolder = core.getInput('dist')
+	const teardown =
+		core.getInput('teardown')?.toString().toLowerCase() === 'true'
+	const prNumber = await getPullRequestNumber()
+	core.debug('github.context')
+	core.debug(JSON.stringify(github.context, null, 2))
+	const { job, payload } = github.context
+	core.debug(`payload.after: ${payload.after}`)
+	core.debug(`payload.after: ${payload.pull_request}`)
+	const gitCommitSha =
+		payload.after ||
+		payload?.pull_request?.head?.sha ||
+		payload?.workflow_run?.head_sha
+	core.debug(JSON.stringify(github.context.repo, null, 2))
+
+	if (!prNumber) {
+		core.info(`😢 No related PR found, skip it.`)
+		return
+	}
+	core.info(`Find PR number: ${prNumber}`)
+
+	const repoOwner = github.context.repo.owner.replace(/\./g, '-')
+	const repoName = github.context.repo.repo.replace(/\./g, '-')
+	const url = previewUrl
+		.replace('{{repoOwner}}', repoOwner)
+		.replace('{{repoName}}', repoName)
+		.replace('{{job}}', job)
+		.replace('{{prNumber}}', `${prNumber}`)
+		.concat('.surge.sh')
+		.concat(previewPath)
+
+	core.setOutput('preview_url', url)
+
+	const buildingLogUrl = await generateLogUrl()
 
 	core.debug(`teardown enabled?: ${teardown}`)
 	core.debug(`event action?: ${payload.action}`)
@@ -146,11 +183,12 @@ async function main() {
 					'https://user-images.githubusercontent.com/507615/98094112-d838f700-1ec3-11eb-8530-381c2276b80e.png',
 			})
 
-			return commentIfNotForkedRepo(
+			return await comment(
 				`:recycle: [PR Preview](https://${url}) ${gitCommitSha} has been successfully destroyed since this PR has been closed. \n ${image}`
 			)
 		} catch (err) {
-			return fail?.(err)
+			core.info('teardown error')
+			return await fail(err)
 		}
 	}
 	const deployingImage = formatImage({
@@ -159,7 +197,7 @@ async function main() {
 			'https://user-images.githubusercontent.com/507615/90240294-8d2abd00-de5b-11ea-8140-4840a0b2d571.gif',
 	})
 
-	commentIfNotForkedRepo(
+	await comment(
 		`⚡️ Deploying PR Preview ${gitCommitSha} to [surge.sh](https://${url}) ... [Build logs](${buildingLogUrl}) \n ${deployingImage}`
 	)
 
@@ -189,15 +227,17 @@ async function main() {
 			command: ['surge', `./${distFolder}`, url, `--token`, surgeToken],
 		})
 
-		commentIfNotForkedRepo(
+		await comment(
 			`🎊 PR Preview ${gitCommitSha} has been successfully built and deployed to https://${url} \n :clock1: Build time: **${duration}s** \n ${image}`
 		)
 	} catch (err) {
-		fail?.(err)
+		core.info('run command error')
+		await fail(err)
 	}
 }
 
 // eslint-disable-next-line github/no-then
-main().catch((err: Error) => {
-	fail?.(err)
+main().catch(async (err: Error) => {
+	core.info('main error')
+	await fail(err)
 })
