7acbd236eb00d397e78dacfa3b5f89b82bb7d02d
Marcos-Toledo <xquinhox@gmail.com>
2023-05-26 10:22:38 -0300

    changing vercel deployment order

--- src/pipeline/shutDown/index.ts
+++ src/pipeline/shutDown/index.ts
@@ -33,17 +33,18 @@ const shutDown = async ({
 		try {
 			core.info(`Teardown: ${mountedUrl}`)
 			core.setSecret(surgeToken)
+
 			await execCommand({
 				command: ['surge', 'teardown', mountedUrl, `--token`, surgeToken],
 			})
+
+			if (vercelToken) await vercelRemoveProjectDeploy(deploymentUrlVercel)
 
 			const image = formatImage({
 				buildingLogUrl,
 				imageUrl:
 					'https://user-images.githubusercontent.com/507615/98094112-d838f700-1ec3-11eb-8530-381c2276b80e.png',
 			})
-
-			if (vercelToken) await vercelRemoveProjectDeploy(deploymentUrlVercel)
 
 			return await comment(
 				`:recycle: [PR Preview](https://${outputUrl}) ${gitCommitSha} has been successfully destroyed since this PR has been closed. \n ${image}`
