cd735d97bc74320c3f1aab95ef2aa67f8da13b41
Marcos-Toledo <xquinhox@gmail.com>
2024-01-22 10:41:37 -0300

    adding command ping

--- src/pipeline/deploy/index.ts
+++ src/pipeline/deploy/index.ts
@@ -1,27 +1,6 @@
-import * as core from '@actions/core'
-import Traceroute from 'nodejs-traceroute-ts'
 import comment from '../../helpers/comment'
 import { deployFinalizedTemplate } from '../../helpers/commentTemplates'
 import { IDeployParams } from './types'
-
-const tracerroute = (path: string) => {
-	const tracer = new Traceroute()
-	tracer
-		.on('pid', (pid) => {
-			core.debug(`pid: ${pid}`)
-		})
-		.on('destination', (destination) => {
-			core.debug(`destination: ${destination}`)
-		})
-		.on('hop', (hop) => {
-			core.debug(`hop: ${JSON.stringify(hop)}`)
-		})
-		.on('close', (code) => {
-			core.debug(`close: code ${code}`)
-		})
-
-	tracer.trace(path)
-}
 
 const deploy = async ({
 	distFolder,
@@ -38,8 +17,6 @@ const deploy = async ({
 				distFolder,
 				mountedUrl: tenant.commandUrl,
 			})
-
-			tracerroute(tenant.commandUrl)
 		}
 	})
 
