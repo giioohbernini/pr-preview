6dddba008ed08aa92e83580ba36fab9bcc06529b
Marcos-Toledo <xquinhox@gmail.com>
2024-02-05 17:55:03 -0300

    using nodejs-traceroute

--- src/tenants/utils/ping.ts
+++ src/tenants/utils/ping.ts
@@ -1,14 +1,36 @@
 import * as core from '@actions/core'
-import ping from 'ping'
+import { BuildTraceroute } from '@juzi/nodejs-traceroute'
+// import ping from 'ping'
 
 const pingStatus = (host: string, tenantName: string) => {
-	setTimeout(() => {
-		// eslint-disable-next-line github/no-then
-		ping.promise.probe(`${host}`).then((res) => {
-			const responseString = JSON.stringify(res)
-			return core.info(`${responseString} = ${tenantName}`)
-		})
-	}, 9000)
+	try {
+		const tracer = BuildTraceroute()
+		tracer
+			.on('pid', (pid) => {
+				core.info(`pid: ${pid}`)
+			})
+			.on('destination', (destination) => {
+				core.info(`destination: ${destination}`)
+			})
+			.on('hop', (hop) => {
+				core.info(`hop: ${JSON.stringify(hop)}`)
+			})
+			.on('close', (code) => {
+				core.info(`close: code ${code}`)
+			})
+
+		tracer.trace(host)
+	} catch (ex) {
+		core.info(ex)
+	}
+
+	// setTimeout(() => {
+	// 	// eslint-disable-next-line github/no-then
+	// 	ping.promise.probe(`${host}`).then((res) => {
+	// 		const responseString = JSON.stringify(res)
+	// 		return core.info(`${responseString} = ${tenantName}`)
+	// 	})
+	// }, 9000)
 }
 
 export default pingStatus
