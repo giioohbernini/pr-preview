dd5d02cbe191a805a09ca674b1217780a1deb7a8
Marcos-Toledo <xquinhox@gmail.com>
2024-10-10 11:10:38 -0300

    changing shutdown comment

--- src/pipeline/shutDown/index.ts
+++ src/pipeline/shutDown/index.ts
@@ -1,20 +1,12 @@
 import * as core from '@actions/core'
 import fail from '../../helpers/fail'
 import comment from '../../helpers/comment'
-import { formatImage } from '../../helpers/formatImage'
 import { IShutDownPrams } from './types'
 
 const shutDown = async ({
-	buildingLogUrl,
 	gitCommitSha,
 	tenantsList,
 }: IShutDownPrams): Promise<void> => {
-	const image = formatImage({
-		buildingLogUrl,
-		imageUrl:
-			'https://user-images.githubusercontent.com/507615/98094112-d838f700-1ec3-11eb-8530-381c2276b80e.png',
-	})
-
 	try {
 		// eslint-disable-next-line github/array-foreach
 		return tenantsList.forEach(async (tenant) => {
@@ -28,7 +20,11 @@ const shutDown = async ({
 			}
 
 			return await comment(
-				`:recycle: [PR Preview](https://${tenant.outputUrl}) ${gitCommitSha} has been successfully destroyed since this PR has been closed. \n ${image}`
+				`
+					:recycle: PR Preview ${gitCommitSha} has been successfully destroyed since this PR has been closed. \n
+					 - Tenant name: ${tenant.tenantName}
+			     - Tenant URL: [${tenant.outputUrl}](https://${tenant.outputUrl})
+			  `
 			)
 		})
 	} catch (err) {
